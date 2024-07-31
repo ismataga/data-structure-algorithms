3ddb8d6a890e8d09639101c95573dfd297c97bbc
ismet-aghamirzayev <ismataga1@gmail.com>
2024-07-31 08:14:30 +0400

    SingleLinkedList.java

---
+++ src/main/java/org/mastercode/datastructurealgoritms/linkedLIst/Node.java
@@ -0,0 +1,6 @@
+package org.mastercode.datastructurealgoritms.linkedLIst;
+
+public class Node {
+    public   int value;
+    public Node next;
+}
---
+++ src/main/java/org/mastercode/datastructurealgoritms/linkedLIst/SingleLinkedList.java
@@ -0,0 +1,46 @@
+package org.mastercode.datastructurealgoritms.linkedLIst;
+
+public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+
+    public int size;
+
+    public Node createSingleLinkedList(int nodeValue) {
+       head = new Node();
+       Node node = new Node();
+       node.next=null;
+       node.value=nodeValue;
+       head=node;
+       tail=node;
+       size=1;
+       return head;
+    }
+
+
+    public void insertInLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value=nodeValue;
+        if(head==null){
+            createSingleLinkedList(nodeValue);
+            return;
+        } else if (location==0) {
+            node.next=head;
+            head=node;
+
+        } else if (location>=size) {
+            node.next=null;
+            tail.next=node;
+            tail=node;
+        }else{
+            Node temp=head;
+            for(int i=0;i<location-1;i++){
+                temp=temp.next;
+            }
+            Node nextNode = temp.next;
+            temp.next=node;
+            node.next=nextNode;
+        }
+        size++;
+    }
+}
