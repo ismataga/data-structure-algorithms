b82010982c0c01e26d46f38b5952872eb08f298c
ismet-aghamirzayev <ismataga1@gmail.com>
2024-10-09 14:05:19 +0400

    subarraydision

--- src/main/java/org/mastercode/datastructurealgoritms/hackerrank/SubarrayDivision.java
+++ src/main/java/org/mastercode/datastructurealgoritms/hackerrank/SubarrayDivision.java
@@ -1,8 +1,17 @@
 package org.mastercode.datastructurealgoritms.hackerrank;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SubarrayDivision {
+    public static void main(String[] args) {
+        List<Integer> s = Arrays.asList(2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1);
+        int d = 18;  // Sum to match
+        int m = 7;  // Length of the sublist
+
+      Integer result = birthday(s, d, m);
+        System.out.println(result);
+    }
     public static int birthday(List<Integer> s, int d, int m) {
         // Write your code here
         int sum = 0;
@@ -17,7 +26,6 @@ public class SubarrayDivision {
                 total++;
             }
             sum = 0;
-            s.remove(i);
         }
 
         return total;
